843034e8d7bcf2f02bed09588c2d60143046b592
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-06-27 18:41:37 +0800

    add broken item

---
+++ Stage4 - Product Gallery/js/index.js
@@ -0,0 +1,22 @@
+// var Show = {
+//     mainshow: true,
+//     pageshow: true,
+// }
+
+// var vm = new Vue({
+//     // el: "#app",
+//     // data: Show,
+//     // // methods: {
+//     // //     detailpage: function(){
+//     // //         $(window).scrollTop(0)
+//     // //         this.mainshow = true
+//     // //         this.pageshow = !this.mainshow
+//     // //     },  
+//     // //     indexpage: function(){
+//     // //         $(window).scrollTop(0)
+//     // //         this.mainshow = true
+//     // //         this.pageshow = !this.mainshow
+//     // //     }
+
+//     // // }
+// })
