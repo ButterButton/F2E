c1eef13ee9a00d27e1a9981ad870a42b5dd2f910
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-06-28 18:37:52 +0800

    add vue.js

--- Stage4 - Product Gallery/js/index.js
+++ Stage4 - Product Gallery/js/index.js
@@ -1,22 +1,34 @@
-// var Show = {
-//     mainshow: true,
-//     pageshow: true,
+$(document).ready(function() {
+        $("#item").hide();
+});
+
+
+
+var Item = {
+    article_title: 'Helios 44M',
+    article_text: 'A lens with personality',
+    article_content: 'The former Soviet Union mimicked the optical structure of the Carl Zeiss Biotar 58/2 and made a series of Helios 44M, a unique vortex effect that is its most distinctive symbol.'
+}
+
+// var Album = {
+
+
 // }
 
-// var vm = new Vue({
-//     // el: "#app",
-//     // data: Show,
-//     // // methods: {
-//     // //     detailpage: function(){
-//     // //         $(window).scrollTop(0)
-//     // //         this.mainshow = true
-//     // //         this.pageshow = !this.mainshow
-//     // //     },  
-//     // //     indexpage: function(){
-//     // //         $(window).scrollTop(0)
-//     // //         this.mainshow = true
-//     // //         this.pageshow = !this.mainshow
-//     // //     }
 
-//     // // }
-// })
+var vm = new Vue({
+    el: '#app',
+    data: Item,
+    methods:{
+        pageview: function(){
+            $(window).scrollTop(0)
+            this.over = false
+            this.page = !this.over
+        },
+        overview: function(){
+            $(window).scrollTop(0)
+            this.over = true
+            this.page = !this.over
+        }
+    }
+})
