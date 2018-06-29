720dd3bc2d66b823b2a9d9494e293e9bc7d9328e
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-06-29 17:22:37 +0800

    Add function - Vue.js (v-if, @click)

--- Stage4 - Product Gallery/js/index.js
+++ Stage4 - Product Gallery/js/index.js
@@ -1,34 +1,48 @@
-$(document).ready(function() {
-        $("#item").hide();
-});
+// $(document).ready(function() {
+//         $("#item").hide();
+// });
 
 
+// $('.theme_img').click(function() {
+//     $('#main').hide();
+//     $('#item').show();
+//     $(window).scrollTop(0);
+// });
 
-var Item = {
-    article_title: 'Helios 44M',
-    article_text: 'A lens with personality',
-    article_content: 'The former Soviet Union mimicked the optical structure of the Carl Zeiss Biotar 58/2 and made a series of Helios 44M, a unique vortex effect that is its most distinctive symbol.'
+
+// $('.sticker').click(function() {
+//     $('#main').show();
+//     $('#item').hide();
+//     $(window).scrollTop(0);
+// });
+
+
+var todata = {
+    title: 'Helios 44M',
+    text: 'A lens with personality',
+    content: 'The former Soviet Union mimicked the optical structure of the Carl Zeiss Biotar 58/2 and made a series of Helios 44M, a unique vortex effect that is its most distinctive symbol.'
 }
-
-// var Album = {
-
-
-// }
 
 
 var vm = new Vue({
     el: '#app',
-    data: Item,
-    methods:{
-        pageview: function(){
+    data: {
+        showview: todata,
+        indexview: true
+    },
+    methods: {
+        toindex: function() {
+            this.indexview = true
+            this.pageview = !this.indexview
             $(window).scrollTop(0)
-            this.over = false
-            this.page = !this.over
+            console.log("index = " + this.indexview)
         },
-        overview: function(){
+        topage: function() {
+            this.indexview = false
+            this.pageview = !this.indexview
             $(window).scrollTop(0)
-            this.over = true
-            this.page = !this.over
+            console.log("index = " + this.indexview)
+
         }
     }
-})
+});
