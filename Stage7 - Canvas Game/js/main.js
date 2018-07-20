014a0f42c7437be93026f4928b942e8ec9dd4706
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-07-20 18:24:33 +0800

    index UI 80%

--- Stage7 - Canvas Game/js/main.js
+++ Stage7 - Canvas Game/js/main.js
@@ -1,44 +1,89 @@
 var canvas = document.getElementById('mycanvas')
 var ctx = canvas.getContext('2d')
 
+// var w = $(window).width()
+// var h = $(window).height()
+var w = 1024
+var h = 768
 var rectangle = '#E7465D'
 var circle = '#F5AF5F'
 var triangle = '#3676BB'
-canvas.width = 600
-canvas.height = 600
+
+canvas.width = w
+canvas.height = h
 
 function draw() {
-  ctx.clearRect(0, 0, 600, 600)
+  ctx.clearRect(0, 0, w, h)
 
   // 繪製格線方便定位
   ctx.beginPath()
-  ctx.fillStyle = 'black'
-  for (var i = 0; i < 12; i++) {
+  ctx.fillStyle = 'white'
+  for (var i = 0; i < 21; i++) {
     let pos = i * 50
     ctx.moveTo(pos, 0)
-    ctx.lineTo(pos, 600)
-    ctx.fillText(pos, pos, 12)
+    ctx.lineTo(pos, h)
+    ctx.fillText(pos, pos, 21)
 
     ctx.moveTo(0, pos)
-    ctx.lineTo(600, pos)
-    ctx.fillText(pos, 12, pos)
+    ctx.lineTo(w, pos)
+    ctx.fillText(pos, 21, pos)
   }
-  ctx.strokeStyle = 'rgba(0,0,0,0.1)'
+  ctx.strokeStyle = 'white'
   ctx.stroke()
 
-  // 圓形
+  // 黃色圓形
   ctx.beginPath()
-  ctx.arc(500, 150, 50, 0, 2 * Math.PI)
+  ctx.arc(850, 150, 50, 0, 2 * Math.PI)
   ctx.stroke()
   ctx.fillStyle = circle
   ctx.fill()
 
+  // 雷達大圓
+  ctx.beginPath()
+  ctx.arc(500, 400, 300, 0, 2 * Math.PI)
+  ctx.strokeStyle = '#FFF'
+  ctx.stroke()
+
+  // 雷達小圓
+  ctx.beginPath()
+  ctx.arc(500, 400, 150, 0, 2 * Math.PI)
+  ctx.strokeStyle = '#FFF'
+  ctx.stroke()
+
+
   // 三角形
   ctx.beginPath()
-  ctx.moveTo(400, 400)
-  ctx.lineTo(350, 500)
-  ctx.lineTo(450, 500)
+  ctx.moveTo(700, 625)
+  ctx.lineTo(650, 675)
+  ctx.lineTo(725, 700)
   ctx.fillStyle = triangle
   ctx.fill()
+
+  // 多邊形
+  var num = 6
+  var radius = 50
+  ctx.beginPath()
+  for (var i = 0; i <= num; i++) {
+    var x = radius * Math.sin(Math.PI / 180 * 360 / num * i) + 150
+    var y = radius * Math.cos(Math.PI / 180 * 360 / num * i) + 150
+    ctx.lineTo(x, y)
+  }
+  ctx.stroke()
+  ctx.fillStyle = rectangle
+  ctx.fill()
+
+  // 文字主題
+  ctx.font = '22px 微軟正黑體'
+  ctx.fillStyle = '#FFF'
+  ctx.fillText(' Radio Defense', 425, 450)
+
+  // 文字介紹
+  ctx.font = '22px 微軟正黑體'
+  ctx.fillStyle = '#FFF'
+  ctx.fillText(' 你身負著運送能量電池的任務', 50, 600)
+  ctx.fillText(' 卻遭到幾何星人的埋伏', 50, 625)
+  ctx.fillText(' 請協助從他們的手中奪回能量電池', 50, 650)
+
+
 }
 draw()
