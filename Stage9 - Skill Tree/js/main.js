6508bb706da7810b90e640622095d4b3bc8c2b9b
xeriok18600 <xeriok18600@gmail.com>
2018-08-02 23:09:39 +0800

    add mouseclick

--- Stage9 - Skill Tree/js/main.js
+++ Stage9 - Skill Tree/js/main.js
@@ -5,3 +5,25 @@ function skill_btn(c) {
     $(c).removeClass('click')
   }
 }
+// 鎖右健
+$(document).ready(function() {
+  $('.list').bind('contextmenu', function(event) {
+    return false
+  })
+})
+
+/*
+  1 = Left   mouse button
+  2 = Centre mouse button
+  3 = Right  mouse button
+*/
+$('.list').mousedown(function(event) {
+  switch (event.which) {
+    case 1:
+      console.log('左鍵')
+      break
+    case 3:
+      console.log('右鍵')
+      break
+  }
+})
