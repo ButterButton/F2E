7021f33e2c061f68249f598623c1a5cccb504fdc
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-08-02 18:10:06 +0800

    modify button click

--- Stage9 - Skill Tree/js/main.js
+++ Stage9 - Skill Tree/js/main.js
@@ -1,14 +1,7 @@
-function skill_btn() {
-  var num = 0
-  $('button').click(function() {
-    num++
-    if (num % 2 != 0) {
-      $(this).addClass('click')
-      console.log(num)
-    } else if (num % 2 == 0) {
-      $(this).removeClass('click')
-    }
-  })
+function skill_btn(c) {
+  if (!$(c).hasClass('click')) {
+    $(c).addClass('click')
+  } else {
+    $(c).removeClass('click')
+  }
 }
-skill_btn()
-
