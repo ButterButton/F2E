6bad73c1a993537ecc772910ea49945a2ef2953a
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-08-08 18:07:29 +0800

    add ajax(Done) , filter(going)

--- Stage2 - Filter/js/index.js
+++ Stage2 - Filter/js/index.js
@@ -1,14 +1,37 @@
-const Flickrapi = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+tainanJson = "../tainanapi.json"
 
+function Tags() {
+
+}
 
 $.ajax({
-    url: Flickrapi,
-    dataType: 'jsonp',
-    data: { format: "json" },
-    success: function(data) {
-        console.log(data); //formatted JSON data
+    type: "GET",
+    dataType: "json",
+    url: tainanJson,
+    success: function (data) {
+        $.each(data, function (index, ele) {
+            $('#select').append(
+                $('<option>', {
+                    text: ele.district
+                })
+            )
+            if (ele.id == "466") {
+                $('.title > h3').append(ele.name)
+                $('.brief > p').append(ele.introduction)
+                $('.list').append(
+                    $('<span>', {
+                        text: ele.category[0]
+                    }),
+                    $('<span>', {
+                        text: ele.category[1]
+                    }),
+                    $('<span>', {
+                        text: ele.category[2]
+                    })
+                )
+                $('.item > .place > span').append(ele.district)
+                $('.item > .tel > span').append(ele.tel)
+            }
+        })
     }
-});
-
-
-
+})
