d285b2b59857488535262ec869259641c99c24b6
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-08-09 18:15:04 +0800

    add removeDuplicates(Done), filter(going), pagination(going)

--- Stage2 - Filter/js/index.js
+++ Stage2 - Filter/js/index.js
@@ -1,6 +1,18 @@
 tainanJson = "../tainanapi.json"
+var arr = new Array()
+var test = new Array()
 
-function Tags() {
+function removeDuplicates() {
+    const districtResult = [...new Set(arr.map(item => JSON.stringify(item.district)))].map(item => JSON.parse(item))
+    for (i = 0; i < districtResult.length; i++) {
+        $('#select').append(
+            $('<option>', {
+                text: districtResult[i]
+            })
+        )
+    }
+    var districtLength = districtResult.length
+    $('#info').append('<h3>' + 'Showing  ' + arr.length + '  results by…' + '</h3>')
 
 }
 
@@ -10,28 +22,17 @@ $.ajax({
     url: tainanJson,
     success: function (data) {
         $.each(data, function (index, ele) {
-            $('#select').append(
-                $('<option>', {
-                    text: ele.district
-                })
-            )
-            if (ele.id == "466") {
-                $('.title > h3').append(ele.name)
-                $('.brief > p').append(ele.introduction)
-                $('.list').append(
-                    $('<span>', {
-                        text: ele.category[0]
-                    }),
-                    $('<span>', {
-                        text: ele.category[1]
-                    }),
-                    $('<span>', {
-                        text: ele.category[2]
-                    })
-                )
-                $('.item > .place > span').append(ele.district)
-                $('.item > .tel > span').append(ele.tel)
+            var inner = {
+                name: ele.name,
+                district: ele.district,
+                category: ele.category
             }
+            arr.push(inner)
+            test.push(inner.category)
+            $('#right_column').append('<div class="block"><div class="main_img"></div><div class="main_content"><div class="title"><h3>' + ele.name + '</h3></div><div class="brief"><p>' + ele.introduction + '</p></div><div class="list"><span>' + ele.category[0] + '</span><span>' + ele.category[1] + '</span><span>' + ele.category[2] + '</span></div><div class="item"><div class="place"><i class="fas fa-map-marker-alt">' + ele.district + '</i><span> </span></div><div class="tel"><i class="fas fa-phone"></i><span>' + ele.tel + ' </span></div></div></div>')
         })
+        console.log(test)
+        removeDuplicates()
+
     }
 })
