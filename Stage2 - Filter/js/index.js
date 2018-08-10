1574efe7505beb45598a1dbe4cd8c2105857d1ae
xeriok18600 <31687242+xeriok18600@users.noreply.github.com>
2018-08-10 18:05:42 +0800

    filter (Done), pagination (going)

--- Stage2 - Filter/js/index.js
+++ Stage2 - Filter/js/index.js
@@ -1,9 +1,33 @@
 tainanJson = "../tainanapi.json"
 var arr = new Array()
-var test = new Array()
 
-function removeDuplicates() {
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: tainanJson,
+        success: function (data) {
+            $.each(data, function (index, ele) {
+                var inner = {
+                    name: ele.name,
+                    introduction: ele.introduction,
+                    district: ele.district,
+                    category: ele.category,
+                    tel: ele.tel
+                }
+                arr.push(inner)
+                $('#right_column').append('<div class="block"><div class="main_img"></div><div class="main_content"><div class="title"><h3>' + ele.name + '</h3></div><div class="brief"><p>' + ele.introduction + '</p></div><div class="list"><span>' + ele.category[0] + '</span><span>' + ele.category[1] + '</span><span>' + ele.category[2] + '</span></div><div class="item"><div class="place"><i class="fas fa-map-marker-alt">' + ele.district + '</i><span> </span></div><div class="tel"><i class="fas fa-phone"></i><span>' + ele.tel + ' </span></div></div></div>')
+            })
+            duplicatesRemove()
+        }
+    })
+})
+
+
+function duplicatesRemove() {
     const districtResult = [...new Set(arr.map(item => JSON.stringify(item.district)))].map(item => JSON.parse(item))
+    const categoryResult = [...new Set(arr.map(item => JSON.stringify(item.category[0])))].map(item => JSON.parse(item))
+    console.log(categoryResult)
     for (i = 0; i < districtResult.length; i++) {
         $('#select').append(
             $('<option>', {
@@ -11,28 +35,33 @@ function removeDuplicates() {
             })
         )
     }
+    for (i = 0; i < categoryResult.length; i++) {
+        $('#form').append('<input type="checkbox" id="' + 'check' + i + 2 + '" value="' + categoryResult[i] + '">' + '</input>' + '<label>' + categoryResult[i] + '</label>' + '<br>')
+    }
     var districtLength = districtResult.length
-    $('#info').append('<h3>' + 'Showing  ' + arr.length + '  results by…' + '</h3>')
-
+    $('#info').append('<h3>' + 'Showing ' + arr.length + ' results by…' + '</h3>')
 }
 
-$.ajax({
-    type: "GET",
-    dataType: "json",
-    url: tainanJson,
-    success: function (data) {
-        $.each(data, function (index, ele) {
-            var inner = {
-                name: ele.name,
-                district: ele.district,
-                category: ele.category
-            }
-            arr.push(inner)
-            test.push(inner.category)
-            $('#right_column').append('<div class="block"><div class="main_img"></div><div class="main_content"><div class="title"><h3>' + ele.name + '</h3></div><div class="brief"><p>' + ele.introduction + '</p></div><div class="list"><span>' + ele.category[0] + '</span><span>' + ele.category[1] + '</span><span>' + ele.category[2] + '</span></div><div class="item"><div class="place"><i class="fas fa-map-marker-alt">' + ele.district + '</i><span> </span></div><div class="tel"><i class="fas fa-phone"></i><span>' + ele.tel + ' </span></div></div></div>')
-        })
-        console.log(test)
-        removeDuplicates()
+// function categoryFilter() {
+//     $(':checkbox').change(function () {
+//         if (this.checked) {
+//             var val = this.value
 
-    }
-})
+//         }
+//     })
+// }
+
+function locationFilter(c) {
+    var txt = $('#select :selected').text()
+    $('#right_column').empty()
+    $('#info').append('<h3>' + 'Showing ' + arr.length + ' results by…' + '</h3>')
+    var locationarr = arr.filter(function (item, index, array) {
+        if (txt == item.district) {
+            $('#right_column').append('<div class="block"><div class="main_img"></div><div class="main_content"><div class="title"><h3>' + item.name + '</h3></div><div class="brief"><p>' + item.introduction + '</p></div><div class="list"><span>' + item.category[0] + '</span><span>' + item.category[1] + '</span><span>' + item.category[2] + '</span></div><div class="item"><div class="place"><i class="fas fa-map-marker-alt">' + item.district + '</i><span> </span></div><div class="tel"><i class="fas fa-phone"></i><span>' + item.tel + ' </span></div></div></div>')
+            console.log(item.district)
+        }
+    })
+}
+
+
+// categoryFilter()
